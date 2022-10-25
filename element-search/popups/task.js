8ccf7b6a81c83d632a6c3245c782d97a71873df2
Centurion <ajlykard@mail.ru>
2022-10-25 17:39:25 +0300

    Способы поиска нужного HTML-элемента

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -8,16 +8,17 @@ modalMain.classList.add("modal_active");
 let informationButton1 = showSuccess.item(0);
 console.log(informationButton1);
 informationButton1.onclick = function () {
+    modalMain.classList.remove("modal_active");
     modalSuccess.classList.add("modal_active");
 };
 
 closed ();
 
  function  closed () {
-array.forEach(function (element) {
-    element.onclick = function () {
-        modalMain.classList.remove("modal_active");
-        modalSuccess.classList.remove("modal_active");
+    array.forEach(function (element) {
+        element.onclick = function () {
+            modalMain.classList.remove("modal_active");
+            modalSuccess.classList.remove("modal_active");
     };
 });
 }

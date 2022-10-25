a02d5310839e83695f33f383ead67144a562f3e0
Centurion <ajlykard@mail.ru>
2022-10-25 12:47:55 +0300

    Способы поиска нужного HTML-элемента

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,10 +3,10 @@ const arrayLink = Array.from(menuLink);
 
 
 arrayLink.forEach((menuLink) => {
-	menuLink.onclick = function () { // ф-ия срабатывает при клике на пункт меню-ссылку
-		if (menuLink.closest('.menu__item').querySelector('.menu_sub')) { // если у ближайшего родителя  нажатой ссылки есть класс menu_sub, то значит это нажали на меню с подменю, а не просто ссылка
-			menuLink.closest('.menu__item').querySelector('.menu_sub').classList.toggle('menu_active'); // переключаем класс чтобы меню то появлялось, то убиралось
-		  return false; // это отмена стандартного поведения браузера при клике на ссылке, чтобы не перезагружалась страница
+	menuLink.onclick = function () { 
+		if (menuLink.closest('.menu__item').querySelector('.menu_sub')) { 
+			menuLink.closest('.menu__item').querySelector('.menu_sub').classList.toggle('menu_active'); 
+		  return false; 
 		};
   };
 });
